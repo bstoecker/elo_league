b33ce18f1adf02f0bbeffec1c29b53720a2d9147
lasong <Lekealem.Asong@invision.de>
2014-11-27 10:00:50 +0100

    put team and users in nested resources of league

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
-    resources :leagues, except: [:new, :edit]
-
-    resources :teams, except: [:new, :edit]
-
-    resources :users, except: [:new, :edit]
+    resources :leagues, except: [:new, :edit] do
+      resources :teams, except: [:new, :edit]
+      resources :users, except: [:new, :edit]
+    end
   end
 end
